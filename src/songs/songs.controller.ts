15365809069e17b319070e56f99bb1868da79abf
Ayobami6 <ayobamidele006@gmail.com>
2024-04-06 18:25:03 +0100

    Implement fething all songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { Song } from './songs.entity';
 
 @Controller('songs')
 export class SongsController {
   constructor(private songService: SongsService) {}
 
   @Get()
-  getAllSongs(): string {
+  getAllSongs(): Promise<Song[]> {
     return this.songService.getAllSongs();
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { Song } from './songs.entity';
 
 @Injectable()
 export class SongsService {
-  getAllSongs(): string {
-    return 'All songs';
+  // lets inject the datasource dependency
+  constructor(private dataSource: DataSource) {}
+
+  // lets override the Repostory methosa
+
+  songRepository = this.dataSource.getRepository(Song).extend({
+    async getSongs(): Promise<Song[]> {
+      return await this.find();
+    },
+  });
+
+  getAllSongs(): Promise<Song[]> {
+    return this.songRepository.getSongs();
   }
 }

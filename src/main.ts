6f0c69b6d5573254ccb830df4253965bd0bfac1a
Ayobami6 <ayobamidele006@gmail.com>
2024-04-06 15:20:15 +0100

    Add logger.log(`Application listening on port ${PORT}`);

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();

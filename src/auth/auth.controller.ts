e442eb81eef3df14ae74148573e401c40a9158d8
Ayobami6 <ayobamidele006@gmail.com>
2024-04-11 09:58:42 +0100

    add new 2fa enable auth endpint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserEntity } from 'src/users/user.entity';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { SignInUserDto } from 'src/users/dto/signIn-credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { Enable2FA } from './interfaces/jwt.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +25,16 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body() signUser: SignInUserDto): Promise<any> {
     return this.authService.signIn(signUser);
+  }
+
+  @Post('enable-2fa')
+  @UseGuards(AuthGuard('jwt'))
+  async enable2fa(@GetUser() user): Promise<Enable2FA> {
+    try {
+      const userId = user.id;
+      return this.authService.enable2FA(userId);
+    } catch (err) {
+      throw new InternalServerErrorException();
+    }
   }
 }
